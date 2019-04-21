b4692c9eaa0b8575ff71e2e9e6399f06ffbf28f6
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2019-04-21 16:57:38 +0800

    🔧 Use wee_alloc as allocator for size reduction

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use wasm_bindgen::prelude::*;
 use uuid::Uuid;
+use wee_alloc;
+
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 
 #[wasm_bindgen]
